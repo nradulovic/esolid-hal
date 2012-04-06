409c24229f31d0f22bbde013d138542fe50d5660
Nenad Radulović <nenad.b.radulovic@gmail.com>
2012-04-06 00:11:11 +0000

    Napisana je inicijalizacija za GPIO sa direktnim pristupom registrima.

--- src/port/variant/stm32l1xx_md/uart_lld.c
+++ src/port/variant/stm32l1xx_md/uart_lld.c
@@ -45,6 +45,15 @@
  *//*--------------------------------------------------------------------------------------------*/
 HAL_DBG_DEFINE_MODULE(UART Low Level Driver);
 
+#define UART1_GPIO_AFIO                 (uint32_t)0x07
+#define UART1_GPIO_AFIO_POS_TX          (uint32_t)4U
+#define UART1_GPIO_AFIO_POS_RX          (uint32_t)8U
+#define UART1_GPIO_PORT                 GPIOA
+#define UART1_GPIO_PIN_TX               (uint32_t)9U
+#define UART1_GPIO_PIN_RX               (uint32_t)10U
+#define GPIO_AFIO_UART2                 (uint32_t)0x07
+#define GPIO_AFIO_UART3                 (uint32_t)0x07
+
 /*============================================================================  LOCAL MACRO's  ==*/
 /*=========================================================================  LOCAL DATA TYPES  ==*/
 
@@ -106,14 +115,22 @@ void lldUartDrvInit(
 
 #if defined(OPT_HAL_UART) && defined(OPT_HAL_UART_USE_1) || defined(__DOXYGEN__)
     uart1drv.regs = USART1;
-    RCC->APB2RSTR |= RCC_APB2RSTR_USART1RST;                                    /* Reset hardvera - aktivno.                                */
-    RCC->APB2RSTR &= ~RCC_APB2RSTR_USART1RST;                                   /* Reset hardvera - neaktivno.                              */
-    RCC->APB2ENR  &= ~RCC_APB2ENR_USART1EN;                                     /* Iskljuci takt.                                           */
-    RCC->APB2LPENR &= ~RCC_APB2LPENR_USART1LPEN;                                /* Iskljuci low-power takt.                                 */
+    RCC->AHBRSTR |= RCC_AHBRSTR_GPIOARST;                                       /* Reset GPIO hardvera - aktivno.                           */
+    RCC->AHBRSTR &= ~RCC_AHBRSTR_GPIOARST;                                      /* Reset GPIO hardvera - neaktivno.                         */
+    RCC->AHBENR  &= ~RCC_AHBENR_GPIOAEN;                                        /* Iskljuci GPIO takt.                                      */
+    RCC->AHBLPENR &= ~RCC_AHBLPENR_GPIOALPEN;                                   /* Iskljuci GPIO lowpower takt                              */
+    RCC->APB2RSTR |= RCC_APB2RSTR_USART1RST;                                    /* Reset UART hardvera - aktivno.                           */
+    RCC->APB2RSTR &= ~RCC_APB2RSTR_USART1RST;                                   /* Reset UART hardvera - neaktivno.                         */
+    RCC->APB2ENR  &= ~RCC_APB2ENR_USART1EN;                                     /* Iskljuci UART takt.                                      */
+    RCC->APB2LPENR &= ~RCC_APB2LPENR_USART1LPEN;                                /* Iskljuci UART low-power takt.                            */
 #endif
 
 #if defined(OPT_HAL_UART) && defined(OPT_HAL_UART_USE_2) || defined(__DOXYGEN__)
     uart2drv.regs = USART2;
+    RCC->AHBRSTR |= RCC_AHBRSTR_GPIOARST;
+    RCC->AHBRSTR &= ~RCC_AHBRSTR_GPIOARST;
+    RCC->AHBENR  &= ~RCC_AHBENR_GPIOAEN;
+    RCC->AHBLPENR &= ~RCC_AHBLPENR_GPIOALPEN;
     RCC->APB1RSTR |= RCC_APB1RSTR_USART2RST;
     RCC->APB1RSTR &= ~RCC_APB1RSTR_USART2RST;
     RCC->APB1ENR  &= ~RCC_APB1ENR_USART2EN;
@@ -122,6 +139,10 @@ void lldUartDrvInit(
 
 #if defined(OPT_HAL_UART) && defined(OPT_HAL_UART_USE_3) || defined(__DOXYGEN__)
     uart3drv.regs = USART3;
+    RCC->AHBRSTR |= RCC_AHBRSTR_GPIOBRST;
+    RCC->AHBRSTR &= ~RCC_AHBRSTR_GPIOBRST;
+    RCC->AHBENR  &= ~RCC_AHBENR_GPIOBEN;
+    RCC->AHBLPENR &= ~RCC_AHBLPENR_GPIOBLPEN;
     RCC->APB1RSTR |= RCC_APB1RSTR_USART3RST;
     RCC->APB1RSTR &= ~RCC_APB1RSTR_USART3RST;
     RCC->APB1ENR  &= ~RCC_APB1ENR_USART3EN;
@@ -135,22 +156,60 @@ void lldUartDrvStart(
 
     USART_InitTypeDef USART_InitStructure;
     NVIC_InitTypeDef NVIC_InitStructure;
-    GPIO_InitTypeDef GPIO_InitStructure;
 
 #if defined(OPT_HAL_UART) && defined(OPT_HAL_UART_USE_1) || defined(__DOXYGEN__)
     if (esUART1 == aUart) {
-        RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+        RCC->AHBENR |= RCC_AHBENR_GPIOAEN;                                      /* Ukljuci GPIO takt.                                       */
+        RCC->APB2ENR |= RCC_APB2ENR_USART1EN;                                   /* Ukljuci UART takt.                                       */
+
+        switch (aUart->drvDef.uartMode) {
+            case ES_UART_MODE_RX_ONLY : {
+                GPIOA->AFR[1] &= ~GPIO_AFRH_AFRH10;                             /* Ocisti AF polje pin-a.                                   */
+                GPIOA->AFR[1] |= (UART1_GPIO_AFIO << UART1_GPIO_AFIO_POS_RX);   /* Postavi potrebnu AF vrednost.                            */
+                GPIOA->MODER &= ~GPIO_MODER_MODER10;                            /* Ocisti polje za rezim rada.                              */
+                GPIOA->MODER |= GPIO_MODER_MODER10_1;                           /* Postavi AF rezim rada pina.                              */
+                GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR10;                      /* Maksimalna brzina rada.                                  */
+                GPIOA->OTYPER &= ~GPIO_OTYPER_OT_10;                            /* Izlaz je u Push-pull rezimu.                             */
+                GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR10;                            /* Ne koriste se pull-up/pull-down otpornici.               */
+                break;
+            }
+
+            case ES_UART_MODE_TX_ONLY : {
+                GPIOA->AFR[1] &= ~GPIO_AFRH_AFRH9;
+                GPIOA->AFR[1] |= (UART1_GPIO_AFIO << UART1_GPIO_AFIO_POS_TX);
+                GPIOA->MODER &= ~GPIO_MODER_MODER9;
+                GPIOA->MODER |= GPIO_MODER_MODER9_1;
+                GPIOA->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR9;
+                GPIOA->OTYPER &= ~GPIO_OTYPER_OT_9;
+                GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR9;
+                break;
+            }
+
+            default : {
+                HAL_DBG_CHECK(ES_UART_MODE_TX_AND_RX == aUart->drvDef.uartMode);
+                GPIOA->AFR[1] &= ~(GPIO_AFRH_AFRH9 | GPIO_AFRH_AFRH10);
+                GPIOA->AFR[1] |= ((UART1_GPIO_AFIO << UART1_GPIO_AFIO_POS_TX) | (UART1_GPIO_AFIO << UART1_GPIO_AFIO_POS_RX));
+                GPIOA->MODER &= ~(GPIO_MODER_MODER9 | GPIO_MODER_MODER10);
+                GPIOA->MODER |= (GPIO_MODER_MODER9_1 | GPIO_MODER_MODER10_1);
+                GPIOA->OSPEEDR |= (GPIO_OSPEEDER_OSPEEDR9 | GPIO_OSPEEDER_OSPEEDR10);
+                GPIOA->OSPEEDR &= ~(GPIO_OTYPER_OT_9 | GPIO_OTYPER_OT_10);
+                GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR9 | GPIO_PUPDR_PUPDR10);
+                break;
+            }
+        }
     }
 #endif
 
 #if defined(OPT_HAL_UART) && defined(OPT_HAL_UART_USE_2) || defined(__DOXYGEN__)
     if (esUART2 == aUart) {
+        RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
         RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
     }
 #endif
 
 #if defined(OPT_HAL_UART) && defined(OPT_HAL_UART_USE_3) || defined(__DOXYGEN__)
     if (esUART2 == aUart) {
+        RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
         RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
     }
 #endif
@@ -158,6 +217,10 @@ void lldUartDrvStart(
 
 }
 
+void lldUartDrvStop(
+    esUartDrv_T     * aUart) {
+
+}
 #endif /* OPT_HAL_UART */
 /*===================================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
 
