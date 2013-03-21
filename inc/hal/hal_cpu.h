68820b9004a2c9fd29435b502f692ca306cb619d
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-03-21 02:03:26 +0000

    - Doxygen formatiranje - Uklonjeni su svi makroi, koriste se samo INLINE funkcije

--- inc/hal/hal_cpu.h
+++ inc/hal/hal_cpu.h
@@ -1,4 +1,4 @@
-/*************************************************************************************************
+/******************************************************************************
  * This file is part of eSolid
  *
  * Copyright (C) 2011, 2012 - Nenad Radulovic
@@ -20,155 +20,187 @@
  *
  * web site:    http://blueskynet.dyndns-server.com
  * e-mail  :    blueskyniss@gmail.com
- *//******************************************************************************************//**
+ *//***********************************************************************//**
  * @file
  * @author      Nenad Radulovic
  * @brief       Interfejs za CPU modul.
- * ------------------------------------------------------------------------------------------------
  * @addtogroup  hal_cpu
- ****************************************************************************************//** @{ */
+ *********************************************************************//** @{ */
 
 #ifndef HAL_CPU_H_
 #define HAL_CPU_H_
 
-/*============================================================================  INCLUDE FILES  ==*/
-/*==================================================================================  DEFINES  ==*/
-/*-------------------------------------------------------------------------------------------*//**
+/*=========================================================  INCLUDE FILES  ==*/
+/*===============================================================  DEFINES  ==*/
+
+/*------------------------------------------------------------------------*//**
  * @name        Atributi za CPU
- * @{ *//*---------------------------------------------------------------------------------------*/
-/*-------------------------------------------------------------------------------------------*//**
+ * @{ *//*--------------------------------------------------------------------*/
+
+/**
  * @brief       Operacije za citanje/upis iz ROM memorije su identicne
  *              operacijama za citanje/upis iz RAM memorije.
  * @details		Kada je ovaj atribut definisan onda kada je memorijski prostor
- * 				procesora jedinstven i pristup podacima u RAM i ROM memoriji je
- * 				identičan. Ovaj atribut	moze da se koristi radi selekcije
- * 				optimalnog algoritma u nekoj funkciji. Primera radi, kod
- * 				starijih mikrokontrolera (kao sto je PIC16 i PIC18 serija)
- * 				operacija čitanja podataka iz flash memorije zahteva najpre
- * 				inicijalizaciju tablicnih registara, a citanje podataka se
- * 				obavlja indirektno, preko registra. Navedene operacije zahtevaju
- * 				odredjeno vreme izvrsavanja i cela operacija postaje vrlo
- * 				neefikasna.
- *//*--------------------------------------------------------------------------------------------*/
+ * 				procesora jedinstven, odnosno, linearan i pristup podacima u RAM
+ * 				i ROM memoriji je identican. Ovaj atribut moze da se koristi
+ * 				radi selekcije optimalnog algoritma u nekoj funkciji.
+ *
+ * 				Primera radi, kod starijih mikrokontrolera (kao sto je PIC16 i
+ * 				PIC18 serija) operacija citanja podataka iz FLASH memorije
+ * 				zahteva najpre inicijalizaciju tablicnih registara i citanje
+ * 				podataka se	obavlja indirektno, preko registra. Navedene
+ * 				operacije zahtevaju odredjeno vreme izvrsavanja i ceo proces je
+ * 				vrlo neefikasan.
+ *
+ * 				Ukoliko je makro:
+ * 				- definisan - CPU ima linearan adresni prostor
+ * 				- nedefinisan - CPU ima odvojene adresne prostore za podatke i
+ * 				program
+ */
 #if defined(__DOXYGEN__)
-# define ES_CPU_ATTRIB_UNITED_ACCESS
+# define ES_CPU_ATTRIB_LINEAR_ADDR
 #endif
 
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Poravnjanje podataka
- *//*--------------------------------------------------------------------------------------------*/
+/**
+ * @brief       Pokazuje da li CPU podrzava pristup podacima koji nisu
+ *              poravnani
+ *
+ *              Ukoliko je makro:
+ *              - definisan - CPU podrzava pristup podacima koji ne moraju da
+ *                  budu poravnani
+ *              - nedefinisan - CPU ne podrzava neporavnani pristup
+ */
+#if defined(__DOXYGEN__)
+# define ES_CPU_ATTRIB_UNALIGNED_ACCESS
+#endif
+
+/**
+ * @brief       Poravnjanje podataka (alignement)
+ */
 #if defined(__DOXYGEN__)
 # define ES_CPU_ATTRIB_ALIGNMENT
 #endif
-/** @} *//*--------------------------------------------------------------------------------------*/
 
-/*-------------------------------------------------------------------------------------------*//**
+/** @} *//*-------------------------------------------------------------------*/
+/*------------------------------------------------------------------------*//**
  * @name        Tip podataka koji odgovara sirini magistrale podataka
  * @brief       Ovi tipovi definisu tipove podataka koji su @b zavisni od
  *              koriscene arhitekture/varijante.
- * @{ *//*---------------------------------------------------------------------------------------*/
+ * @{ *//*--------------------------------------------------------------------*/
 #if !defined(ES_CPU_NATIVE_BITS) || defined(__DOXYGEN__)
-# define ES_CPU_NATIVE_BITS             8U
-typedef int8_t                          native_T;
+# define ES_CPU_NATIVE_BITS
 #endif
 
 #if !defined(ES_CPU_UNATIVE_BITS) || defined(__DOXYGEN__)
-# define ES_CPU_UNATIVE_BITS            8U
-typedef uint8_t                         unative_T;
-#endif
-/** @} *//*--------------------------------------------------------------------------------------*/
-
-/*==================================================================================  MACRO's  ==*/
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Zaustavi procesor
- * @details     Ovaj makro se obicno poziva u slucaju neke katastrofalne greske
- *              i treba zaustaviti dalji rad procesora. Kada se jednom ovaj
- *              makro pokrene on se ne moze prekinuti.
- *//*--------------------------------------------------------------------------------------------*/
-#if !defined(ES_CPU_STOP) || defined(__DOXYGEN__)
-# define ES_CPU_STOP()                                                         \
-    while (1)
-
+# define ES_CPU_UNATIVE_BITS
 #endif
 
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Prebaci rezim rada u stanje uspavano
- * @details     Ovaj makroi prepacuje mikrokontroler u stanje najmanje potrosnje.
- *              U zavisnosti od implementacije mikrokontrolera deo periferije se,
- *              takodje, iskljucuje.
- *//*--------------------------------------------------------------------------------------------*/
-#if !defined(ES_CPU_SLEEP) || defined(__DOXYGEN__)
-# define ES_CPU_SLEEP()                                                         \
-    (void)0
+/** @} *//*-------------------------------------------------------------------*/
+/*===============================================================  MACRO's  ==*/
 
-#endif
-
-/*-------------------------------------------------------------------------------------------*//**
- * @name        Brza manipulacija sa bitovima
- * @{ *//*---------------------------------------------------------------------------------------*/
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Find Last set Bit makro
- * @param       expr1                   Broj koji se ispituje.
- * @return      y = log2(expr1)
- *//*--------------------------------------------------------------------------------------------*/
-#if defined(__DOXYGEN__)
-# define ES_CPU_FLS(expr1)
-#endif
-
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Find First set Bit makro
- * @param       expr1                   Broj koji se ispituje.
- *//*--------------------------------------------------------------------------------------------*/
-#if defined(__DOXYGEN__)
-# define ES_CPU_FFS(expr1)
-#endif
-
-/** @} *//*--------------------------------------------------------------------------------------*/
-/*-------------------------------------------------------------------------------------------*//**
+/*------------------------------------------------------------------------*//**
  * @name        Manipulacija SysTick brojaca.
  * @brief       Makroi za rad sa SysTick brojacem.
- * @{ *//*---------------------------------------------------------------------------------------*/
-/*-------------------------------------------------------------------------------------------*//**
+ * @{ *//*--------------------------------------------------------------------*/
+
+/**
  * @brief       Izbor kloka koji se dovodi SysTick brojacu.
  * @param       clksource               Klok koji se dovodi SysTick modulu.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 #if !defined(HAL_SYSTICK_CLOCK_CONFIG) || defined(__DOXYGEN__)
 # define HAL_SYSTICK_CLOCK_CONFIG(clksource)
 #endif
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Uzimanje trenutne vrednosti broja tick-ova SysTick modula.
  * @return      Trenutna vrednost broja tick-ova u jedinici vremena.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 #if !defined(HAL_SYSTICK_GET_TICK) || defined(__DOXYGEN__)
 # define HAL_SYSTICK_GET_TICK()             0U
 #endif
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Izbor broja tick-ova SysTick modula.
  * @param       tick                	Broj tick-ova koje generise SysTick modul.
  * @return      1 ako nije korektna tick vrednost ili 0 ako jeste.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 #if !defined(HAL_SYSTICK_CONFIG) || defined(__DOXYGEN__)
 # define HAL_SYSTICK_CONFIG(tick)
 #endif
 
-/*-------------------------------------------------------------------------------------------*//**
+/**
  * @brief       Izvrsi inicijalizaciju SysTick.
  * @param       clksource               Klok koji se dovodi SysTick modulu.
- *//*--------------------------------------------------------------------------------------------*/
+ */
 #if !defined(HAL_SYSTICK_CLOCK_CONFIG) || defined(__DOXYGEN__)
 # define HAL_SYSTICK_CLOCK_CONFIG(clksource)
 #endif
 
-/** @} *//*--------------------------------------------------------------------------------------*/
+/** @} *//*-------------------------------------------------------------------*/
+/*============================================================  DATA TYPES  ==*/
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*===================================================  FUNCTION PROTOTYPES  ==*/
 
-/*===============================================================================  DATA TYPES  ==*/
-/*=========================================================================  GLOBAL VARIABLES  ==*/
-/*======================================================================  FUNCTION PROTOTYPES  ==*/
-/*===================================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/**
+ * @brief       Zaustavi procesor
+ * @details     Poziva se kada treba zaustaviti dalji rad procesora.
+ * @note        Kada se kompajlira @c release verzija koda funkcija postavlja
+ *              procesor u stanje @c sleep.
+ * @inline
+ */
+#if defined(__DOXYGEN__)
+C_INLINE_ALWAYS void esCpuStop(
+    void);
+#endif
 
-/** @endcond *//** @} *//*************************************************************************
+/**
+ * @brief       Prebaci rezim rada u stanje uspavano
+ * @details     Ovaj makroi prepacuje mikrokontroler u stanje najmanje potrosnje.
+ *              Periferija koja generise dogadjaje mora da nastavi da radi. U
+ *              slucaju da mikrokontroler ne podrzava rad periferije dok je CPU
+ *              u uspavanom rezimu funkcija nece postaviti MCU u stanje male
+ *              potrosnje.
+ * @note        Kada se kompajlira @c debug verzija koda funkcija ne postavlja
+ *              procesor u stanje @c sleep jer obicno @c sleep stanja procesora
+ *              gase i vezu sa debagerom. Zbog toga se funkcija kompajlira u
+ *              NOP instrukciju i omogucava nesmetano debagiranje.
+ * @inline
+ */
+#if defined(__DOXYGEN__)
+C_INLINE_ALWAYS void esCpuSleep(
+    void);
+#endif
+
+/**
+ * @brief       Find Last set Bit
+ * @param       value                   Broj koji se ispituje.
+ * @return      y = log2(expr1)
+ * @pre         value != 0
+ * @details     Primer 1: parametar @c value ima vrednost sa binarnom
+ *              reprezentacijom: 110. Funkcija u ovom slucaju vraca vrednost 2.
+ *              Primer 2: parameter @c value ima vrednost sa binarnom
+ *              reprezentacijom: 101001. Funkcija u ovom slucaju vraca vrednost
+ *              5.
+ * @inline
+ */
+#if defined(__DOXYGEN__)
+C_INLINE_ALWAYS uint_fast8_t esCpuFindLastSet(
+    uint32_t value);
+#endif
+
+/**
+ * @brief       Find First set Bit
+ * @param       value                   Broj koji se ispituje.
+ * @return      Vraca poziciju zadnjeg bita koji je setovan u @c value.
+ * @inline
+ */
+#if defined(__DOXYGEN__)
+C_INLINE_ALWAYS uint_fast8_t esCpuFindFirstSet(
+    uint32_t value);
+#endif
+
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//******************************************************
  * END of hal_cpu.h
- *************************************************************************************************/
+ ******************************************************************************/
 #endif /* HAL_CPU_H_ */
