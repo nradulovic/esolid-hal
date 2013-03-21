47b8ca8c4d07c265597e1ce51d33d53ffe47b6b2
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-03-21 02:00:11 +0000

    - BUG FIX: stm32f100rbt6 nije LD_VL serija već MD_VL!

--- inc/port/family/stm32f10x_profile.h
+++ inc/port/family/stm32f10x_profile.h
@@ -1,4 +1,4 @@
-/*************************************************************************************************
+/******************************************************************************
  * This file is part of eSolid
  *
  * Copyright (C) 2011, 2012 - Nenad Radulovic
@@ -20,91 +20,202 @@
  *
  * web site:    http://blueskynet.dyndns-server.com
  * e-mail  :    blueskyniss@gmail.com
- *//******************************************************************************************//**
+ *//***********************************************************************//**
  * @file
  * @author  	Nenad Radulovic
  * @brief       Profil familije za STM32F10x seriju.
- * ------------------------------------------------------------------------------------------------
  * @addtogroup  hal_stm32f10x
- ****************************************************************************************//** @{ */
+ *********************************************************************//** @{ */
 
 
 #ifndef STM32F10X_PROFILE_H_
 #define STM32F10X_PROFILE_H_
 
-/*============================================================================  INCLUDE FILES  ==*/
+/*============================================================================*
+ *
+ * STM32F10X_LD
+ *
+ *============================================================================*/
+#if defined(STM32F10X_LD)
+/*=========================================================  INCLUDE FILES  ==*/
+/*===============================================================  DEFINES  ==*/
 
-/*-----------------------------------------------------------------------------------------------*
- * STM32F10X_LD_VL
- *-----------------------------------------------------------------------------------------------*/
-#if defined(STM32F10X_LD_VL)
-#include "stm32f10x.h"
+/**
+ * @brief       Definisanje konstante - familija
+ */
+# define ES_HAL_MCU_FAMILY              "STM32F10x Low density devices"
 
-/*-------------------------------------------------------------------------------------------*//**
- * @name        Konstante mogucnosti HAL modula
- * @{ *//*---------------------------------------------------------------------------------------*/
-/* #define ES_HAL_FEATURE_GPIO */
-/* #define ES_HAL_FEATURE_UART */
-/* #define ES_HAL_FEATURE_TIMER */
-/* #define ES_HAL_FEATURE_CRC */
-/** @} *//*--------------------------------------------------------------------------------------*/
+/**
+ * @brief       Definisanje PORT konstante - familija
+ */
+# define PORT_FAM_STM32F10X_LD
 
 # if defined(OPT_HAL_GPIO)
-# error "HAL: GPIO module is not implented for this family."
-/* #  define ES_HAL_ENABLE_GPIO */
-/* #  include "port/family/stm32f10x/gpio_lld.h" */
+#  error "HAL: GPIO module is not implented for this family."
+#  define ES_HAL_ENABLE_GPIO
 # endif
 
 # if defined(OPT_HAL_UART)
-# error "HAL: UART module is not implented for this family."
-/* #  define ES_HAL_ENABLE_UART */
-/* #  include "port/family/stm32f10x/uart_lld.h" */
+#  error "HAL: UART module is not implented for this family."
+#  define ES_HAL_ENABLE_UART
 # endif
 
 # if defined(OPT_HAL_TIMER)
-# error "HAL: TIMER module is not implented for this family."
-/* #  define ES_HAL_ENABLE_TIMER */
-/* #  include "port/family/stm32f10x/timer_lld.h" */
+#  error "HAL: TIMER module is not implented for this family."
+#  define ES_HAL_ENABLE_TIMER
 # endif
 
 # if defined(OPT_ENABLE_CRC)
-# error "HAL: CRC module is not implented for this family."
-/* #  define ES_HAL_ENABLE_CRC */
-/* #  include "port/family/stm32f10x/crc_lld.h" */
+#  error "HAL: CRC module is not implented for this family."
+#  define ES_HAL_ENABLE_CRC
 # endif
+
+/*===============================================================  MACRO's  ==*/
+/*============================================================  DATA TYPES  ==*/
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*===================================================  FUNCTION PROTOTYPES  ==*/
+#endif /* STM32F10X_LD */
+
+/*============================================================================*
+ *
+ * STM32F10X_LD_VL
+ *
+ *============================================================================*/
+#if defined(STM32F10X_LD_VL)
+/*=========================================================  INCLUDE FILES  ==*/
+/*===============================================================  DEFINES  ==*/
+
+/**
+ * @brief       Definisanje konstante - familija
+ */
+# define ES_HAL_MCU_FAMILY                                                      \
+    "STM32F10x Low density Value Line devices"
+
+/**
+ * @brief       Definisanje PORT konstante - familija
+ */
+# define PORT_FAM_STM32F10X_LD_VL
+
+# if defined(OPT_HAL_GPIO)
+#  error "HAL: GPIO module is not implented for this family."
+#  define ES_HAL_ENABLE_GPIO
+# endif
+
+# if defined(OPT_HAL_UART)
+#  error "HAL: UART module is not implented for this family."
+#  define ES_HAL_ENABLE_UART
+# endif
+
+# if defined(OPT_HAL_TIMER)
+#  error "HAL: TIMER module is not implented for this family."
+#  define ES_HAL_ENABLE_TIMER
+# endif
+
+# if defined(OPT_ENABLE_CRC)
+#  error "HAL: CRC module is not implented for this family."
+#  define ES_HAL_ENABLE_CRC
+# endif
+
+/*===============================================================  MACRO's  ==*/
+/*============================================================  DATA TYPES  ==*/
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*===================================================  FUNCTION PROTOTYPES  ==*/
 #endif /* STM32F10X_LD_VL */
 
-/*==================================================================================  DEFINES  ==*/
-/*-------------------------------------------------------------------------------------------*//**
+/*============================================================================*
+ *
+ * STM32F10X_MD_VL
+ *
+ *============================================================================*/
+#if defined(STM32F10X_MD_VL)
+/*=========================================================  INCLUDE FILES  ==*/
+/*===============================================================  DEFINES  ==*/
+
+/**
+ * @brief       Definisanje konstante - familija
+ */
+# define ES_HAL_MCU_FAMILY                                                      \
+    "STM32F10x Medium density Value Line devices"
+
+/**
+ * @brief       Definisanje PORT konstante - familija
+ */
+# define PORT_FAM_STM32F10X_MD_VL
+
+# if defined(OPT_HAL_GPIO)
+#  error "HAL: GPIO module is not implented for this family."
+#  define ES_HAL_ENABLE_GPIO
+# endif
+
+# if defined(OPT_HAL_UART)
+#  error "HAL: UART module is not implented for this family."
+#  define ES_HAL_ENABLE_UART
+# endif
+
+# if defined(OPT_HAL_TIMER)
+#  error "HAL: TIMER module is not implented for this family."
+#  define ES_HAL_ENABLE_TIMER
+# endif
+
+# if defined(OPT_ENABLE_CRC)
+#  error "HAL: CRC module is not implented for this family."
+#  define ES_HAL_ENABLE_CRC
+# endif
+
+/*===============================================================  MACRO's  ==*/
+/*============================================================  DATA TYPES  ==*/
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*===================================================  FUNCTION PROTOTYPES  ==*/
+#endif /* STM32F10X_MD_VL */
+
+/*============================================================================*
+ *
+ * COMMON
+ *
+ *============================================================================*/
+/*=========================================================  INCLUDE FILES  ==*/
+
+#if defined(ES_HAL_ENABLE_GPIO)
+# include "port/family/stm32f10x/gpio_lld.h"
+#endif
+
+#if defined(ES_HAL_ENABLE_UART)
+# include "port/family/stm32f10x/uart_lld.h"
+#endif
+
+#if defined(ES_HAL_ENABLE_TIMER)
+# include "port/family/stm32f10x/timer_lld.h"
+#endif
+
+#if defined(ES_HAL_ENABLE_CRC)
+# include "port/family/stm32f10x/crc_lld.h"
+#endif
+
+/*===============================================================  DEFINES  ==*/
+
+/*------------------------------------------------------------------------*//**
  * @name        Opisne konstante
  * @brief       Ovde se nalaze konstante koje daju vise informacija o
  *              mikrokontroleru i proizvodjacu.
- * @{ *//*---------------------------------------------------------------------------------------*/
+ * @{ *//*--------------------------------------------------------------------*/
 #define ES_HAL_MCU_MANUF                "STMicroelectronics"
-/** @} *//*--------------------------------------------------------------------------------------*/
 
-/*-----------------------------------------------------------------------------------------------*
- * STM32F10X_LD_VL
- *-----------------------------------------------------------------------------------------------*/
-#if defined(STM32F10X_LD_VL)
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Definisanje konstante - familija
- *//*--------------------------------------------------------------------------------------------*/
-# define ES_HAL_MCU_FAMILY              "STM32F10x Medium Density Value Line"
+/** @} *//*-------------------------------------------------------------------*/
+/*------------------------------------------------------------------------*//**
+ * @name        Konstante mogucnosti HAL modula
+ * @{ *//*--------------------------------------------------------------------*/
+#define ES_HAL_FEATURE_GPIO
+#define ES_HAL_FEATURE_UART
+#define ES_HAL_FEATURE_TIMER
+#define ES_HAL_FEATURE_CRC
 
-/*-------------------------------------------------------------------------------------------*//**
- * @brief       Definisanje PORT konstante - familija
- *//*--------------------------------------------------------------------------------------------*/
-# define PORT_FAM_STM32F10X_LD_VL
-#endif /* STM32F10X_LD_VL */
-
-/*==================================================================================  MACRO's  ==*/
-/*===============================================================================  DATA TYPES  ==*/
-/*=========================================================================  GLOBAL VARIABLES  ==*/
-/*======================================================================  FUNCTION PROTOTYPES  ==*/
-/*===================================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
-
-/** @endcond *//** @} *//*************************************************************************
+/** @} *//*-------------------------------------------------------------------*/
+/*===============================================================  MACRO's  ==*/
+/*============================================================  DATA TYPES  ==*/
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*===================================================  FUNCTION PROTOTYPES  ==*/
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//******************************************************
  * END of stm32f10x_profile.h
- *************************************************************************************************/
+ ******************************************************************************/
 #endif /* STM32F10X_PROFILE_H_ */
