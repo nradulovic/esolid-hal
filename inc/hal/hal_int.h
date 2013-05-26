fca802393ee31ee32f0a008d81af3fa8aaa9377e
Nenad Radulović <nenad.b.radulovic@gmail.com>
2013-05-26 15:13:03 +0000

    - DELETE: macros for interrupt management

--- inc/hal/hal_int.h
+++ inc/hal/hal_int.h
@@ -77,6 +77,34 @@ enum esInterruptPrio {
     ES_PRIO_REALTIME = 255
 };
 
+/*------------------------------------------------------------------------*//**
+ * @name        Ulazak i izlazak iz kriticnih sekcija koda
+ * @brief       Kriticne sekcije koda su zasticene od prekida ciji su prioriteti
+ *              manji ili jednaki od prioriteta tekuce izvrsavanog koda.
+ * @{ *//*--------------------------------------------------------------------*/
+
+/**
+ * @brief       Deklaracija promenljive u kojoj se cuva trenutnu masku
+ *              prioriteta tekuceg koda.
+ * @details     Ovaj makro se koristi u kombinaciji sa makroima za ulazak i
+ *              izlazak iz kriticnih sekcija koda.
+ */
+# define ES_CRITICAL_DECL()
+
+/**
+ * @brief       Ulazak u kriticni deo koda, postavlja se najvisi dozvoljeni
+ *              prioritet za eSolid
+ * @param       prio                    Prioritet kriticne sekcije koda.
+ *  @arg        0 - 255                 postavljanje maske.
+ */
+# define ES_CRITICAL_ENTER(prio)
+
+/**
+ * @brief       Izlazak iz kriticnog dela koda, vraca se prethodni prioritet.
+ */
+# define ES_CRITICAL_EXIT()
+
+/** @} *//*-------------------------------------------------------------------*/
 #endif /* defined(__DOXYGEN__) */
 /*------------------------------------------------------  C++ extern begin  --*/
 #ifdef __cplusplus
@@ -135,7 +163,7 @@ void esIntPrioMaskSet(
  *  @retval     0                       maska je iskljucena,
  *  @retval     1 - 255                 vrednost maske.
  */
-uint32_t esIntPrioMaskGet(
+esIntPrioReg_T esIntPrioMaskGet(
     void);
 
 /** @} *//*-------------------------------------------------------------------*/
